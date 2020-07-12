891e29f22de40f9dca42072743aa8cf8a1e38007
simpsonyuki <yukilin0327@gmail.com>
2020-07-12 13:02:39 +0800

    0712 設定第二區塊

--- GoToElement.js
+++ GoToElement.js
@@ -1,14 +1,21 @@
 window.onload = () =>{
     console.log("頁面載入完成~");
-
+    // 取得元素
     var test = document.getElementById("test");
-
+    // 元素.點擊事件
     test.onclick = () => {
-        var target = test.getAttribute("data-gte-target");
-        var offset = test.getAttribute("data-gte-offset");
+        var target = test.getAttribute("data-gte-target");   //目標屬性
+        var offset = test.getAttribute("data-gte-offset");   //目標位移
 
-        console.log(target);
-        console.log(offset);
+        // console.log(target);
+        // console.log(offset);
 
+        var element = document.getElementById(target);   //目標元素
+
+        // 視窗.捲動至({上方, 行為})
+        window.scrollTo({
+            top: element.offsetTop - offset,   //取得上方位移(減掉的offset是html裡設定的data-gte-offset的值, 會讓區塊在移動的時候不會直接頂到視窗頂端,而是會多留data-gte-offset設定的位移高度)
+            behavior: "smooth",   //平滑移動
+        })
     }
 }
