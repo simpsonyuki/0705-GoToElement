153867657ad548d42b08d63c05afc4fa1ba4395c
simpsonyuki <yukilin0327@gmail.com>
2020-07-12 14:13:16 +0800

    0712 完成可移動到指定區塊

--- GoToElement.js
+++ GoToElement.js
@@ -1,21 +1,33 @@
 window.onload = () =>{
     console.log("頁面載入完成~");
-    // 取得元素
-    var test = document.getElementById("test");
-    // 元素.點擊事件
-    test.onclick = () => {
-        var target = test.getAttribute("data-gte-target");   //目標屬性
-        var offset = test.getAttribute("data-gte-offset");   //目標位移
+    // 取得單一元素
+    // var test = document.getElementById("test");
 
-        // console.log(target);
-        // console.log(offset);
+    // 取得元素: 透過標籤取得所有(*)元素
+    var elements = document.getElementsByTagName("*");
 
-        var element = document.getElementById(target);   //目標元素
-
-        // 視窗.捲動至({上方, 行為})
-        window.scrollTo({
-            top: element.offsetTop - offset,   //取得上方位移(減掉的offset是html裡設定的data-gte-offset的值, 會讓區塊在移動的時候不會直接頂到視窗頂端,而是會多留data-gte-offset設定的位移高度)
-            behavior: "smooth",   //平滑移動
-        })
+    for (let index = 0; index < elements.length; index++) {
+        const e = elements[index];
+        
+        // 元素.點擊事件
+        e.onclick = () => {
+            var target = e.getAttribute("data-gte-target");   //目標屬性
+            var offset = e.getAttribute("data-gte-offset");   //目標位移
+    
+            // console.log(target);
+            // console.log(offset);
+    
+            if(target){
+                
+                var element = document.getElementById(target);   //目標元素
+        
+                // 視窗.捲動至({上方, 行為})
+                window.scrollTo({
+                    top: element.offsetTop - offset,   //取得上方位移(減掉的offset是html裡設定的data-gte-offset的值, 會讓區塊在移動的時候不會直接頂到視窗頂端,而是會多留data-gte-offset設定的位移高度)
+                    behavior: "smooth",   //平滑移動
+                })
+            }
+        }
     }
+
 }
